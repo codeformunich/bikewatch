859a764d72c2269fdc616f5a17b5ae4ee74f76c6
Lukas VM <lukas@debvm>
2016-11-12 16:09:06 +0100

    use date and time parameters from view

--- evaluation/static/evaluation/appView.cls.js
+++ evaluation/static/evaluation/appView.cls.js
@@ -8,12 +8,34 @@ function appView() {
     
     //this.errorFunction = app.prototype.errorFunction;
     
+    this.evaluateParams = function() {
+        var form = document.querySelector("#" + this.controlDiv + " form");
+        var date = form.date.value;
+        var time = form.time.value;
+        this.mapManager.customParams = encodeURI(date) + '/' + encodeURI(time);
+        console.log(this.mapManager.customParams);
+    };
+
+    this.refresh = function() {
+        this.evaluateParams();
+        this.mapManager.refreshMap();
+    };
+
     this.onNavbarLoaded = function() { /* has to be defined beofre runapp */
+        var thiz = this;
+
+        // bind change event handlers
+        var form = document.querySelector("#" + this.controlDiv + " form");
+        $(form.date).bind('dp.change', function(){ thiz.refresh(); });
+        // TODO find out event for slider
+        //$(form.timo).change(function(){ thiz.refresh(); });
+
+        // init map
+        this.evaluateParams();
         this.mapManager.createMap(this.mapDiv,heatMapLayer);
     }
     
     this.runApp = function (mapDiv,controlDiv) {
-        this.mapManager.customParams = '2016.10.10/16';
         app.prototype.runApp.call(this,mapDiv,controlDiv);
     }
     
