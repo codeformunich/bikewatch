e48a0a178d39ded89cb5da268d88239445101be7
lacinoire <laci_noire@live.de>
2016-11-12 01:14:47 +0100

    include bootstrap in bower

--- mvgrad/settings.py
+++ mvgrad/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'hackatum',
         'USER': 'hackatum',
         'PASSWORD': 'hackatum2016',
-        'HOST': '10.0.0.1',
+        'HOST': '131.159.216.58',
     }
 }
 
@@ -136,5 +136,6 @@ BOWER_COMPONENTS_ROOT = BASE_DIR + '/components'
 
 BOWER_INSTALLED_APPS = (
 'heatmap.js.bower#^2.0.5',
-'leaflet'
+'leaflet',
+'bootstrap'
 )
